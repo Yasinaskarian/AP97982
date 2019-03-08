0e46cf0e2e8e17c315af9e362e4d7b7530d2b529
Yasin Askarian <askarian98@gmail.com>
2019-03-08 22:13:50 +0330

    Add base file

---
+++ A1S1/A1S1Tests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A1S1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1S1.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MainTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
