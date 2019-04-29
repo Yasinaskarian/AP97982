4b015b1dcb1560d7302cec1bc69db5b2d8c3b2cb
Yasin Askarian <askarian98@gmail.com>
2019-04-29 12:55:43 +0430

    finished

---
+++ E1B/E1B/BasicQuestions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    public class BasicQuestions
+    {
+        public static int OddSum(int[] nums)
+        {
+            int sum = 0;
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 != 0)
+                    sum += nums[i];
+
+            }
+            return sum;
+        }
+
+        public static void Swap(ref int a, ref int b)
+        {
+            int t= a;
+            a = b;
+            b = t;
+        }
+
+        public static void Swap(ref double a, ref double b)
+        {
+            double t = a;
+            a = b;
+            b = t;
+        }
+
+        public static void Swap(ref long a, ref long b)
+        {
+             long t = a;
+            a = b;
+            b = t;
+        }
+    }
+}
---
+++ E1B/E1B/Human.cs
@@ -0,0 +1,44 @@
+﻿namespace E1B
+{
+    public interface IHasAge
+    {
+       int GetAge();
+    }
+    public class Human: IHasAge
+    {
+        string _name;
+        public string Name
+        {
+            set
+            {
+                this._name = value;
+            }
+            get
+            {
+                return this._name;
+            }
+        }
+        int _age;
+        public int Age
+        {
+            set
+            {
+                this._age = value;
+            }
+            get
+            {
+                return this._age;
+            }
+        }
+        public Human(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+       public int GetAge()
+        {
+            return Age;
+        }
+    }
+}
