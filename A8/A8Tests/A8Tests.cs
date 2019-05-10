4412055ac8b1512b155eaa2eaa0393d471a5ec1f
Yasin Askarian <askarian98@gmail.com>
2019-05-10 21:31:52 +0430

    finished

---
+++ A8/A8Tests/A8Tests.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using A8;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A8.Tests
+{
+    [TestClass()]
+    public class A8Tests
+    {
+        [TestMethod()]
+        public void HumanTest()
+        {
+            DateTime date1 = new DateTime(1999, 3, 1, 7, 0, 0);
+            Human Yasin = new Human("yasin", "askarian", date1, 188);
+            Assert.AreEqual("yasin", Yasin.FirstName);
+            Assert.AreEqual("askarian", Yasin.LastName);
+            Assert.AreEqual(date1, Yasin.BirthDate);
+            Assert.AreEqual(188, Yasin.Height);
+            Assert.AreNotEqual(DateTime.Today, Yasin.BirthDate);
+            Assert.AreNotEqual("Mozhgan", Yasin.FirstName);
+        }
+        [TestMethod()]
+        public void plus()
+        {
+            DateTime date1 = new DateTime(2008, 3, 1, 7, 0, 0);
+            Human h1 = new Human("yasin", "askarian", date1, 188);
+            object o = "yasin";
+            Human Person = new Human("ChildFirstName", "ChildLastName", DateTime.Today, 30);
+            Assert.AreEqual(Person, (h1 + o));
+            Assert.AreNotEqual(h1, (h1 + o));
+        }
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            DateTime date1 = new DateTime(2008, 3, 1, 7, 0, 0);
+            DateTime date2 = new DateTime(2000, 3, 1, 7, 0, 0);
+            Human h1 = new Human("ali", "askarian", DateTime.Today, 188);
+            Human h2 = new Human("yasin", "askarian", date1, 188);
+            Human h3 = new Human("yasin", "askarian", DateTime.Today, 158);
+            Human h4 = new Human("yasin", "askarian", date2, 88);
+            Human h5 = new Human("ali", "askarian", DateTime.Today, 188);
+            Assert.AreEqual(true, Equals(h5, h1));
+            Assert.AreEqual(false, Equals(h1, h2));
+            Assert.AreEqual(true, h4 > h2);
+            Assert.AreEqual(true, h1 < h2);
+            Assert.AreEqual(true, h1 == h3);
+            Assert.AreEqual(false, h1 != h3);
+            Assert.AreEqual(true, h1 >= h3);
+            Assert.AreEqual(true, h1 <= h3);
+            
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+            DateTime date1 = new DateTime(2008, 3, 1, 7, 0, 0);
+            Human h1 = new Human("ali", "askarian", DateTime.Today, 188);
+            Human h2 = new Human("yasin", "askarian", date1, 188);
+            Human h3 = new Human("yasin", "askarian", date1, 188);
+            Assert.AreNotEqual(h1.GetHashCode(), h2.GetHashCode());
+            Assert.AreEqual(h2.GetHashCode(), h3.GetHashCode());
+        }
+    }
+}
