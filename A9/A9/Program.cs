a520d403d3da1cb13859751f0c58f672dfb7625d
Yasin Askarian <askarian98@gmail.com>
2019-05-18 18:19:10 +0430

    finished

---
+++ A9/A9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    
+    }
+}
