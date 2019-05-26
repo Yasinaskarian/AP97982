737434afc768e5e0a838319e792826f0b78efd0b
Yasin Askarian <askarian98@gmail.com>
2019-05-26 13:28:17 +0430

    finished

---
+++ A11/A11/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A11
+{
+    public class Account
+    {
+        private double _balance;
+        public double Balance
+        {
+            get
+            {
+                if (_balance == null)
+                    throw new NullReferenceException();
+                return _balance;
+
+            }
+            set
+            {
+                if (value == null)
+                    throw new NullReferenceException();
+                _balance = value;
+            }
+        }
+
+        public Account(double balance)
+        {
+            if (balance < 0)
+            {
+                balance = 0;
+                Console.WriteLine($"Initial balance is invalid. Setting balance to 0.");
+            }
+            else
+             Balance = balance;
+        }
+        public virtual void Credit(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Credit amount must be positive");
+            }
+            Balance += amount;
+        }
+        public virtual bool Debit(double amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+                return true;
+            }
+            else
+            {
+                
+                Console.WriteLine($"Debit amount exceeded account balance.");
+                return false;
+            }
+        }
+    }
+}
