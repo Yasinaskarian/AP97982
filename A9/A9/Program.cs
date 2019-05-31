6282582dac25227bed1de9f1933ffe309dcb905c
Yasin Askarian <askarian98@gmail.com>
2019-05-31 19:24:08 +0430

    review time

--- A9/A9/Program.cs
+++ A9/A9/Program.cs
@@ -11,9 +11,6 @@ namespace A9
     {
         public static void Main(string[] args)
         {
-            ExceptionHandler eh = new ExceptionHandler("s", false);
-            eh.Input = null;
-            Console.WriteLine(eh.Input);
         }
     
     }
