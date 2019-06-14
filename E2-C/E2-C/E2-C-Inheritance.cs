9d94146cd8e9b0434ac88e11ecdd59331a8037be
Yasin Askarian <askarian98@gmail.com>
2019-06-14 22:09:27 +0430

    first step

---
+++ E2-C/E2-C/E2-C-Inheritance.cs
@@ -0,0 +1,94 @@
+﻿namespace E2
+{
+    public abstract class Person
+    {
+        public virtual string Name { get; set; }
+        public bool IsFemale { get; set; }
+        public Person(string name = null, bool isFemale = false)
+        {
+            if (isFemale == true)
+            {
+                this.Name = $"خانم {name}";
+            }
+            else
+                this.Name = $"آقای {name}";
+
+            this.IsFemale = isFemale;
+        }
+        public abstract int LunchRate { get; }
+
+    }
+
+    public class Student : Person
+    {
+        public string Name { get; set; }
+        public bool IsFemale { get; set; }
+        public override int LunchRate => 2000;
+        //public Student(string name, bool isFemale)
+        //    : base(name, isFemale);
+        public Student(string name, bool isFemale)
+            : base(name, isFemale)
+        {
+            //if (isFemale == true)
+            //{
+            //    this.Name = $"خانم {name}";
+            //}
+            //else
+            //    this.Name = $"آقای {name}";
+
+            //this.IsFemale = isFemale;
+        }
+
+    }
+
+    public class Employee : Person
+    {
+        public override int LunchRate => 5000;
+        public string Name { get; set; }
+        public bool IsFemale { get; set; }
+        //public Student(string name, bool isFemale)
+        //    : base(name, isFemale);
+        public Employee(string name, bool isFemale)
+        {
+            string s = null;
+            if (isFemale == true)
+            {
+                s = $"خانم {name}";
+            }
+            else
+                s = $"آقای {name}";
+
+            this.IsFemale = isFemale;
+            this.Name = s;
+        }
+
+        public virtual int CalculateSalary(int v)
+        {
+            return v * 5000;
+        }
+    }
+
+    public class Teacher: Employee
+    {
+        public override int LunchRate => 10000;
+        public string Name { get; set; }
+        public bool IsFemale { get; set; }
+        public Teacher(string name, bool isFemale)
+            : base(name, isFemale)
+        {
+            if (isFemale == true)
+            {
+                this.Name = $"استاد {name}";
+            }
+            else
+                this.Name = $"استاد {name}";
+
+            this.IsFemale = isFemale;
+            Name = this.Name;
+        }
+        public override int CalculateSalary(int v)
+        {
+            return v * 20000;
+        }
+    }
+}
---
+++ E2-C/E2-C/E2-C-MessageAnalysis.cs
@@ -0,0 +1,151 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using Microsoft.VisualBasic.FileIO;
+
+namespace E2.Linq
+{
+    public class MessageAnalysis
+    {
+        public List<MessageData> Messages { get; set; }
+
+        public MessageAnalysis()
+        {
+            Messages = new List<MessageData>();
+        }
+
+        public static MessageAnalysis MessageAnalysisFactory(string csvAddress)
+        {
+            MessageAnalysis messageAnalysis = new MessageAnalysis();
+            using (TextFieldParser parser = new TextFieldParser(csvAddress))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                var fields = parser.ReadFields();
+
+                while (!parser.EndOfData)
+                {
+                    fields = parser.ReadFields();
+                    messageAnalysis.AppendMessage(fields);
+                }
+            }
+
+            return messageAnalysis;
+        }
+
+        public void AppendMessage(string[] fields)
+        {
+            Messages.Add(new MessageData(fields));
+        }
+
+        public MessageData MostRepliedMessage()
+        {
+            List<int?> repid = Messages
+                 .Where(d => d.ReplyMessageId != null)
+                 .Select(d=>d.ReplyMessageId)
+                 .ToList();
+            List<int> id = Messages
+                 .Select(d => d.Id)
+                 .ToList();
+
+            MessageData max = Messages[0];
+            int countmax = 0;
+            int count = 0;
+            for(int i = 0; i < id.Count; i++)
+            {
+                count = 0;
+                for (int j = 0; j < repid.Count; j++)
+                {
+                    if (id[i] == repid[j])
+                        count++;
+                }
+                if (count > countmax)
+                {
+                    countmax = count;
+                    max = Messages[i];
+                }
+
+            }
+
+            return max;
+               
+
+
+            
+        }
+
+        public Tuple<string, int>[] MostPostedMessagePersons()
+        {
+            string[] s = Messages
+                .Where(d => d.Author != "Sauleh Eetemadi")
+                .Where(d => d.Author != "Ali Heydari")
+                .GroupBy(g => g.Author)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Key)
+                .Take(5)
+                .ToArray();
+
+            int[] i = Messages
+                .Where(d => d.Author != "Sauleh Eetemadi")
+                .Where(d => d.Author != "Ali Heydari")
+                .GroupBy(g => g.Author)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Count())
+                .Take(5)
+                .ToArray();
+            //»?‘ —?‰ Å?«„ Amir Esmaeili
+            //»«  ⁄œ«œ131 Å?«„ 
+
+            Tuple<string, int>[] t = new Tuple<string, int>[s.Length];
+            for (int a = 0; a < s.Length; a++)
+            {
+                var q = Tuple.Create(s[a], i[a]);
+                t[a] = q;
+            }
+            return t;
+        }
+
+        public Tuple<string, int>[] MostActivesAtMidNight()
+        {
+            string[] s = Messages
+                 .Where(d => (d.DateTime.Hour <= 04)&&(d.DateTime.Hour>=00))
+                  .GroupBy(g => g.Author)
+                  .OrderByDescending(g => g.Count())
+                  .Select(g => g.Key)
+                  .Take(5)
+                  .ToArray();
+
+            int[] i = Messages
+                .Where(d => (d.DateTime.Hour <= 04) && (d.DateTime.Hour >= 00))
+                .GroupBy(g => g.Author)
+                .OrderByDescending(g => g.Count())
+                .Select(g => g.Count())
+                .Take(5)
+                .ToArray();
+            //»?‘ —?‰ »Â  — ?» Amir Esmaeili ali heydari sauleh eetemadi hossein amin mir
+            //»?‘ —?‰ »Â  — ?»  ⁄œ«œ 11Ê8Ê5Ê3Ê3
+            Tuple<string, int>[] t = new Tuple<string, int>[s.Length];
+            for (int a = 0; a < s.Length; a++)
+            {
+                var q = Tuple.Create(s[a], i[a]);
+                t[a] = q;
+            }
+            return t;
+        }
+
+        public string StudentWithMostUnansweredQuestions()
+        {
+            string[] s = Messages
+    .Where(d => d.Content.Contains("?") || d.Content.Contains("ø"))
+    .GroupBy(g => g.Author)
+    .OrderByDescending(g => g.Count())
+    .Select(g => g.Key)
+    .ToArray();
+            //»?‘ —?‰ Amir Esmaeli
+            return s[0];
+        }
+    }
+}
