1847cb6cc570dabcc445048129ab9b051316dbd9
Yasin Askarian <askarian98@gmail.com>
2019-08-06 13:31:10 +0430

    add pdfs

---
+++ E1B/E1B/Project/BasicQuestions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E1
+{
+    public class BasicQuestions
+    {
+        public static int OddSum(int[] nums)
+        {}
+
+        public static void Swap(ref int a, ref int b)
+        { }
+
+        public static void Swap(ref double a, ref double b)
+        { }
+
+        public static void Swap(ref long a, ref long b)
+        { }
+    }
+}
